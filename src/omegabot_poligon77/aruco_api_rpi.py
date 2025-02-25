0b59fe406940b0c77f3e667b151b2cda00a86e3a
Boris Volkov <volkov.boria2003@mail.ru>
2025-02-25 10:12:15 +0300

    Повышена отказоустойчивость видеопотока

--- src/omegabot_poligon77/aruco_api_rpi.py
+++ src/omegabot_poligon77/aruco_api_rpi.py
@@ -25,76 +25,80 @@ class omegabot_aruco:
 
     def bot_aruco_recog(self):
         # Подключение к видеостриму
-        self.cap = cv2.VideoCapture(self.input)
-        #self.cap = cv2.VideoCapture('output_recorded_aruco.avi')
-        #cap = cv2.VideoCapture('rtsp://10.1.100.42:8554/picam_h264')
-
-        if not self.cap.isOpened():
-            print("Не удалось открыть видеопоток")
-        else:
-            dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50) #Стандартный словарь 4x4
-            parameters = aruco.DetectorParameters()
-            font = cv2.FONT_HERSHEY_SIMPLEX
+        try:
             while True:
-                ret, frame = self.cap.read()  # Чтение кадра из потока
-                if not ret:
-                    break
-                # Преобразуем кадр в оттенки серого
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.cap = cv2.VideoCapture(self.input)
+                #self.cap = cv2.VideoCapture('output_recorded_aruco.avi')
+                #cap = cv2.VideoCapture('rtsp://10.1.100.42:8554/picam_h264')
 
-                # Create the ArUco detector
-                detector = aruco.ArucoDetector(dictionary, parameters)
-                # Распознаём маркеры
-                corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
-                
-                self.dictionary_lock.acquire()
-                if not ids is None:
-                        lenght = 0
+                if not self.cap.isOpened():
+                    print("Не удалось открыть видеопоток")
+                else:
+                    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50) #Стандартный словарь 4x4
+                    parameters = aruco.DetectorParameters()
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    while True:
+                        ret, frame = self.cap.read()  # Чтение кадра из потока
+                        if not ret:
+                            break
+                        # Преобразуем кадр в оттенки серого
+                        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-                        for id in range(0, ids.size):
-                            if str(ids.item(id)) not in self.check_dictionary.keys():
-                                self.check_dictionary[str(ids.item(id))] = [2, 0, 0, 0]
-                            else:
-                                if self.check_dictionary[str(ids.item(id))][0] < 50:
-                                    self.check_dictionary[str(ids.item(id))][0] = self.check_dictionary[str(ids.item(id))][0] + 2
+                        # Create the ArUco detector
+                        detector = aruco.ArucoDetector(dictionary, parameters)
+                        # Распознаём маркеры
+                        corners, ids, rejectedImgPoints = detector.detectMarkers(gray)
+                        
+                        self.dictionary_lock.acquire()
+                        if not ids is None:
+                                lenght = 0
 
-                            ##mesaga = #mesaga + str(ids.item(id)) + "("
-                            width_x = abs((corners[id][0][0][0] - corners[id][0][2][0]))
-                            middle_x = (corners[id][0][0][0] + corners[id][0][2][0])/2
-                            middle_y = (corners[id][0][0][1] + corners[id][0][2][1])/2
-                            width_y = abs(corners[id][0][0][1] - corners[id][0][2][1])
-                            lenght = math.sqrt(pow(width_x,2)+pow(width_y,2))
-                            ##mesaga = #mesaga + str(int(middle_x)) + "," + str(int(middle_y)) + "," + str(int(lenght)) + ")"
-                            self.check_dictionary[str(ids.item(id))][1] = int(middle_x)
-                            self.check_dictionary[str(ids.item(id))][2] = int(middle_y)
-                            self.check_dictionary[str(ids.item(id))][3] = int(lenght)
+                                for id in range(0, ids.size):
+                                    if str(ids.item(id)) not in self.check_dictionary.keys():
+                                        self.check_dictionary[str(ids.item(id))] = [2, 0, 0, 0]
+                                    else:
+                                        if self.check_dictionary[str(ids.item(id))][0] < 50:
+                                            self.check_dictionary[str(ids.item(id))][0] = self.check_dictionary[str(ids.item(id))][0] + 2
 
-                            #if self.check_dictionary[str(ids.item(id))][0] > 10:
-                                #cv2.linine(frame,(int(corners[id][0][0][0]),int(corners[id][0][0][1])),(int(corners[id][0][1][0]),int(corners[id][0][1][1])),(100,0,0),5)
-                                #cv2.linine(frame,(int(corners[id][0][2][0]),int(corners[id][0][2][1])),(int(corners[id][0][3][0]),int(corners[id][0][3][1])),(0,0,100),5)
-                                #cv2.linine(frame,(int(corners[id][0][0][0]),int(corners[id][0][0][1])),(int(corners[id][0][2][0]),int(corners[id][0][2][1])),(100,100,100),5)
+                                    ##mesaga = #mesaga + str(ids.item(id)) + "("
+                                    width_x = abs((corners[id][0][0][0] - corners[id][0][2][0]))
+                                    middle_x = (corners[id][0][0][0] + corners[id][0][2][0])/2
+                                    middle_y = (corners[id][0][0][1] + corners[id][0][2][1])/2
+                                    width_y = abs(corners[id][0][0][1] - corners[id][0][2][1])
+                                    lenght = math.sqrt(pow(width_x,2)+pow(width_y,2))
+                                    ##mesaga = #mesaga + str(int(middle_x)) + "," + str(int(middle_y)) + "," + str(int(lenght)) + ")"
+                                    self.check_dictionary[str(ids.item(id))][1] = int(middle_x)
+                                    self.check_dictionary[str(ids.item(id))][2] = int(middle_y)
+                                    self.check_dictionary[str(ids.item(id))][3] = int(lenght)
 
-                #mesaga = ""   
-                for_destroy = []         
-                for key in self.check_dictionary.keys():
-                    lenght = self.check_dictionary[key][3]
-                    middle_x = self.check_dictionary[key][1]
-                    middle_y = self.check_dictionary[key][2]
-                    self.check_dictionary[key][0] = self.check_dictionary[key][0] - 1
-                    if self.check_dictionary[key][0] > 10:
-                        pass
-                        #cv2.circle(frame,(int(middle_x),int(middle_y)), int(lenght/10), (255,0,255), -1)
-                        #cv2.putText(frame,key,(int(middle_x),int(middle_y)), font, 4,(255,255,255), 5, #cv2.linINE_AA)
-                        #mesaga = #mesaga + "(" + key + "," + str(self.check_dictionary[key]) + ")"
-                    if self.check_dictionary[key][0] <= 0:
-                        for_destroy.append(key)
-                for key in for_destroy:
-                    del self.check_dictionary[key]
-                self.dictionary_lock.release()
-                #print(#mesaga)
-                # Рисуем границы вокруг распознанных маркеров
-                #frame = aruco.drawDetectedMarkers(frame, corners, ids)
-                #cv2.imshow('Video Stream', frame)  # Отображение кадра
+                                    #if self.check_dictionary[str(ids.item(id))][0] > 10:
+                                        #cv2.linine(frame,(int(corners[id][0][0][0]),int(corners[id][0][0][1])),(int(corners[id][0][1][0]),int(corners[id][0][1][1])),(100,0,0),5)
+                                        #cv2.linine(frame,(int(corners[id][0][2][0]),int(corners[id][0][2][1])),(int(corners[id][0][3][0]),int(corners[id][0][3][1])),(0,0,100),5)
+                                        #cv2.linine(frame,(int(corners[id][0][0][0]),int(corners[id][0][0][1])),(int(corners[id][0][2][0]),int(corners[id][0][2][1])),(100,100,100),5)
+
+                        #mesaga = ""   
+                        for_destroy = []         
+                        for key in self.check_dictionary.keys():
+                            lenght = self.check_dictionary[key][3]
+                            middle_x = self.check_dictionary[key][1]
+                            middle_y = self.check_dictionary[key][2]
+                            self.check_dictionary[key][0] = self.check_dictionary[key][0] - 1
+                            if self.check_dictionary[key][0] > 10:
+                                pass
+                                #cv2.circle(frame,(int(middle_x),int(middle_y)), int(lenght/10), (255,0,255), -1)
+                                #cv2.putText(frame,key,(int(middle_x),int(middle_y)), font, 4,(255,255,255), 5, #cv2.linINE_AA)
+                                #mesaga = #mesaga + "(" + key + "," + str(self.check_dictionary[key]) + ")"
+                            if self.check_dictionary[key][0] <= 0:
+                                for_destroy.append(key)
+                        for key in for_destroy:
+                            del self.check_dictionary[key]
+                        self.dictionary_lock.release()
+                        #print(#mesaga)
+                        # Рисуем границы вокруг распознанных маркеров
+                        #frame = aruco.drawDetectedMarkers(frame, corners, ids)
+                        #cv2.imshow('Video Stream', frame)  # Отображение кадра
+        except:
+            print("Подулючаемся к стриму повторно")
 
 
     #Функция проверки наличия маркера в поле зрения
